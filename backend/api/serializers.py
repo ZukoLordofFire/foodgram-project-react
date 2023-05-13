6181941a0a0cae9fd5bf575f06d04b1dc8c8704a
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-14 02:46:09 +0300

    front

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -158,7 +158,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
         read_only_fields = 'is_following',
 
     def get_is_following(self, obj):
-        user = self.context.get('request').user
+        user = self.context.get('request')
         if user.is_anonymous:
             return False
         return Follow.objects.filter(user=user, author=obj.id).exists()
