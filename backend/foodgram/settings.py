2cea5de8f8c970a9db569127d647b666aa4600ab
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-13 17:58:26 +0300

    front

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
