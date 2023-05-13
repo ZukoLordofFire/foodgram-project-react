8c14a73e5fc54e8a8968e1dbfa401d6bfa569b87
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-14 02:14:16 +0300

    front

--- backend/api/views.py
+++ backend/api/views.py
@@ -153,8 +153,9 @@ class UserViewSet(POSTandGETViewSet):
         permission_classes=(IsAuthenticated,)
     )
     def me(self, request):
+        user = request.user
         if request.method == 'GET':
-            serializer = CustomUserSerializer(user=self.request.user)
+            serializer = CustomUserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
