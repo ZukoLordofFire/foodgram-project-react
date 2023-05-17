54d4bcc488329efcafa46332f5a5a859eab90e51
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-17 17:16:25 +0300

    front

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -11,8 +11,7 @@ class AdminOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_permission(self, request, view):
         return (
             request.method in SAFE_METHODS
-            or (request.user.is_authenticated
-                and request.user.is_staff)
+            or request.user and request.user.is_staff
         )
 
 
