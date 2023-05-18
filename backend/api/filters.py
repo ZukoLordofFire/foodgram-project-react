dffa7a6c9059a29c312b1dba914f110a85bd2721
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-18 03:54:31 +0300

    front

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,9 +1,6 @@
 import django_filters
-from django.contrib.auth import get_user_model
 from recipes.models import Recipe, Tag
 from rest_framework.filters import SearchFilter
-
-User = get_user_model
 
 
 class IngredientSearchFilter(SearchFilter):
@@ -11,9 +8,10 @@ class IngredientSearchFilter(SearchFilter):
 
 
 class AuthorAndTagFilter(django_filters.FilterSet):
-    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
-    author = django_filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
+                                                    field_name="tags__slug",
+                                                    to_field_name="slug",)
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author')
+        fields = ('tags',)
