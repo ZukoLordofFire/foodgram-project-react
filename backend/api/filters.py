103b29dc7170aedafba1cae67b5ebaa4a12f629a
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-18 05:58:50 +0300

    front

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -16,6 +16,10 @@ class RecipeFilter(django_filters.FilterSet):
     is_in_shopping_cart = django_filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
 
+    class Meta:
+        model = Recipe
+        fields = ('tags',)
+
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
@@ -27,7 +31,3 @@ class RecipeFilter(django_filters.FilterSet):
         if value and user.is_authenticated:
             return queryset.filter(in_carts__user=user)
         return queryset
-
-    class Meta:
-        model = Recipe
-        fields = ('tags',)
