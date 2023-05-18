48c0992ace7ac04ac3fc047724223bbdc5957947
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-18 05:19:54 +0300

    front

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,20 +12,20 @@ class IngredientSearchFilter(SearchFilter):
 
 class AuthorAndTagFilter(django_filters.FilterSet):
     tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
-                                                    field_name="tags__slug",)
+                                                    field_name="tag__slug",)
     is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = django_filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
-        if value and self.request.user.is_authenticated:
+        if value and user.is_authenticated:
             return queryset.filter(favourites__user=user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if value and self.request.user.is_authenticated:
+        if value and user.is_authenticated:
             return queryset.filter(cart_owner__user=user)
         return queryset
 
