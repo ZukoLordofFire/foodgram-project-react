7b127a7310f7cd50743c2d77e4607c6c0f5fd0ef
Валентин Веселов <valentin-veselov@mail.ru>
2023-05-20 20:57:38 +0300

    review2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -158,8 +158,8 @@ class IngredientAmount(models.Model):
         validators=(
             MinValueValidator(
                 0,
-                'Вы не можете положить 0 чего бы то ни было'
-            )
+                'Вы не можете положить 0 чего бы то ни было',
+            ),
         ),
     )
     ingredient = models.ForeignKey(
